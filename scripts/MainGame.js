b6f19ce18e309ecbfb8f9ea09a48a3999e2e7f82
BenediktEyjolfsson <t1505741@student.southstaffs.ac.uk>
2017-06-29 11:36:23 +0100

    error changes

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -36,7 +36,7 @@ BasicGame.Game.prototype = {
         //add the starfield and logo on screen
         this.starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
         //Add the ship on to the screen, set physics and the boundaries
-        ship = this.add.sprite((this.world.width / 2), this.world.height - 50, 'ship');
+        ship = this.add.sprite((this.worl.width / 2), this.world.height - 50, 'ship');
         ship.anchor.setTo(0.5, 0);
         this.physics.enable(ship, Phaser.Physics.ARCADE);
         ship.body.collideWorldBounds = true;
